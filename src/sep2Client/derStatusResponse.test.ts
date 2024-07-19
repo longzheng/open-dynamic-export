8d169506f9136f23ecd08fbf08a4c38aa2e0114a
Long Zheng <long.zheng@gmail.com>
2024-07-19 14:15:36 +1000

    Add DerStatusResponse

---
+++ src/sep2Client/derStatusResponse.test.ts
@@ -0,0 +1,36 @@
+import { it, expect } from 'vitest';
+import { convertToXml } from './builder';
+import {
+    generateDerStatusResponse,
+    OperationalModeStatus,
+} from './derStatusResponse';
+import { ConnectStatus } from './connectStatus';
+
+it('should generate DERStatus XML', () => {
+    const response = generateDerStatusResponse({
+        readingTime: new Date(1682475028 * 1000),
+        operationalModeStatus: {
+            dateTime: new Date(1682475028 * 1000),
+            value: OperationalModeStatus.OperationalMode,
+        },
+        genConnectStatus: {
+            dateTime: new Date(1682475028 * 1000),
+            value: ConnectStatus.Connected,
+        },
+    });
+
+    const xml = convertToXml(response);
+
+    expect(xml).toBe(`<?xml version="1.0"?>
+<DERStatus xmlns="urn:ieee:std:2030.5:ns">
+    <readingTime>1682475028</readingTime>
+    <operationalModeStatus>
+        <dateTime>1682475028</dateTime>
+        <value>2</value>
+    </operationalModeStatus>
+    <genConnectStatus>
+        <dateTime>1682475028</dateTime>
+        <value>1</value>
+    </genConnectStatus>
+</DERStatus>`);
+});
