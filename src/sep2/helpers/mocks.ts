3382e152635e81eec95f3678d9edf3b77f9bba8b
Long Zheng <long.zheng@gmail.com>
2024-07-19 21:44:01 +1000

    Fix mock file path

--- src/sep2/helpers/mocks.ts
+++ src/sep2/helpers/mocks.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs';
 import path from 'path';
 
 export function getMockFile(file: string): string {
-    const filePath = `${path.join(__dirname, '..', '..', '/tests/sep2/mocks/')}${file}`;
+    const filePath = `${path.join(__dirname, '..', '..', '..', '/tests/sep2/mocks/')}${file}`;
 
     return readFileSync(filePath, 'utf-8');
 }
