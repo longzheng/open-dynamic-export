e73438fd35bedcc06e2dc65bea8f1365f2885506
Long Zheng <long.zheng@gmail.com>
2024-07-22 10:18:55 +1000

    Add inverter block enums

--- src/sunspec/models/inverterBlock.ts
+++ src/sunspec/models/inverterBlock.ts
@@ -1,5 +1,5 @@
 import type { SunSpecBrand } from './brand';
-import { froniusInverterBlock } from './fronius/froniusInverterBlock';
+import { froniusInverterBlock } from './fronius/inverterBlock';
 
 // https://sunspec.org/wp-content/uploads/2015/06/SunSpec-Specification-Inverter-Monitoring-Models-A12020-1.3.pdf
 export type InverterBlock = {
@@ -78,11 +78,11 @@ export type InverterBlock = {
     // Temperature Scale factor
     I_Temp_SF: number;
     // Descriptive Operating State
-    I_Status: number;
+    I_Status: InverterStatus;
     // Descriptive Vendor Defined Operating State
     I_Status_Vendor: number;
     // Descriptive Event Flags (bits 0-31)
-    I_Event_1: number;
+    I_Event_1: InverterEvent1;
     // Descriptive Event Flags (bits 32-63)
     I_Event_2: number;
     // Descriptive Vendor Defined Event Flags (bits 0-31)
@@ -104,4 +104,59 @@ export function getInverterBlockByBrand(brand: SunSpecBrand) {
         case 'solaredge':
             throw new Error('Not implemented');
     }
+}
+
+enum InverterStatus {
+    // Off
+    I_STATUS_OFF = 1,
+    // Sleeping (auto-shutdown)
+    I_STATUS_SLEEPING = 2,
+    // Starting up
+    I_STATUS_STARTING = 3,
+    // Tracking power point
+    I_STATUS_MPPT = 4,
+    // Forced power reduction
+    I_STATUS_THROTTLED = 5,
+    // Shutting down
+    I_STATUS_SHUTTING_DOWN = 6,
+    // One or more faults exist
+    I_STATUS_FAULT = 7,
+    // Standby (service on unit)* might be in Events
+    I_STATUS_STANDBY = 8,
+}
+
+// SunSpec_Information_Model_Reference_20210302.xlsx
+// GROUND_FAULT	0
+// DC_OVER_VOLT	1
+// AC_DISCONNECT	2
+// DC_DISCONNECT	3
+// GRID_DISCONNECT	4
+// CABINET_OPEN	5
+// MANUAL_SHUTDOWN	6
+// OVER_TEMP	7
+// OVER_FREQUENCY	8
+// UNDER_FREQUENCY	9
+// AC_OVER_VOLT	10
+// AC_UNDER_VOLT	11
+// BLOWN_STRING_FUSE	12
+// UNDER_TEMP	13
+// MEMORY_LOSS	14
+// HW_TEST_FAILURE	15
+export enum InverterEvent1 {
+    GROUND_FAULT = 1 << 0,
+    DC_OVER_VOLT = 1 << 1,
+    AC_DISCONNECT = 1 << 2,
+    DC_DISCONNECT = 1 << 3,
+    GRID_DISCONNECT = 1 << 4,
+    CABINET_OPEN = 1 << 5,
+    MANUAL_SHUTDOWN = 1 << 6,
+    OVER_TEMP = 1 << 7,
+    OVER_FREQUENCY = 1 << 8,
+    UNDER_FREQUENCY = 1 << 9,
+    AC_OVER_VOLT = 1 << 10,
+    AC_UNDER_VOLT = 1 << 11,
+    BLOWN_STRING_FUSE = 1 << 12,
+    UNDER_TEMP = 1 << 13,
+    MEMORY_LOSS = 1 << 14,
+    HW_TEST_FAILURE = 1 << 15,
 }
