6d7dcf8cbc065a1629be6a614682b7a9f27e80ba
Long Zheng <long.zheng@gmail.com>
2024-08-28 10:45:33 +1000

    Tweak logging

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,13 +1,18 @@
-import pino from 'pino';
+import pino, { stdTimeFunctions } from 'pino';
 
 export const logger = pino({
     level: 'trace',
+    base: null, // disable PID and hostname
+    timestamp: stdTimeFunctions.isoTime, // write ISO time to log file
     transport: {
         targets: [
             {
                 target: 'pino-rotating-file-stream',
                 level: 'debug',
-                options: { filename: 'debug.log', path: `./logs/` },
+                options: {
+                    filename: 'debug.log',
+                    path: `./logs/`,
+                },
             },
             {
                 target: 'pino-pretty',
