aa928da61f23e24da65d8b464b7152f4417c18c7
Long Zheng <long.zheng@gmail.com>
2024-09-06 16:04:20 +1000

    Add support for certificate SFDI

--- scripts/certDeviceLfdi.ts
+++ scripts/certDeviceLfdi.ts
@@ -1,10 +1,17 @@
 import 'dotenv/config';
-import { getCertificateLfdi } from '../src/sep2/helpers/cert';
+import {
+    getCertificateFingerprint,
+    getCertificateLfdi,
+    getCertificateSfdi,
+} from '../src/sep2/helpers/cert';
 import { getConfig } from '../src/helpers/config';
 import { getSep2Certificate } from '../src/helpers/sep2Cert';
 
 const config = getConfig();
 const sep2Certificate = getSep2Certificate(config);
-const lfdi = getCertificateLfdi(sep2Certificate.cert);
+const fingerprint = getCertificateFingerprint(sep2Certificate.cert);
+const lfdi = getCertificateLfdi(fingerprint);
+const sfdi = getCertificateSfdi(fingerprint);
 
-console.log(lfdi);
+console.log(`LFDI: ${lfdi}`);
+console.log(`SFDI: ${sfdi}`);
