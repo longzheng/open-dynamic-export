987e7adfe1e89f1da9c9d0fac12ca13f1efd0d40
Long Zheng <long.zheng@gmail.com>
2024-09-24 21:53:02 +1000

    Add OperationId to prevent clashing names in OpenAPI generator

--- src/server/controllers/inverterController.ts
+++ src/server/controllers/inverterController.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Route, Tags } from 'tsoa';
+import { Controller, Get, OperationId, Route, Tags } from 'tsoa';
 import { coordinatorService } from '../services/coordinatorService.js';
 
 @Tags('inverterController')
 @Route('inverterController')
 export class InverterController extends Controller {
     @Get('data')
+    @OperationId('inverterControllerData')
     public data() {
         return coordinatorService.inverterControllerData();
     }
