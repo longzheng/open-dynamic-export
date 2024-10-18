afe579dfab9d90f96a59edbff151b2ae707f37b6
Long Zheng <long.zheng@gmail.com>
2024-10-18 17:17:12 +1100

    Remove blog links

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -64,7 +64,6 @@ const config: Config = {
                     position: 'left',
                     label: 'Tutorial',
                 },
-                { to: '/blog', label: 'Blog', position: 'left' },
                 {
                     href: 'https://github.com/facebook/docusaurus',
                     label: 'GitHub',
@@ -104,10 +103,6 @@ const config: Config = {
                 {
                     title: 'More',
                     items: [
-                        {
-                            label: 'Blog',
-                            to: '/blog',
-                        },
                         {
                             label: 'GitHub',
                             href: 'https://github.com/facebook/docusaurus',
