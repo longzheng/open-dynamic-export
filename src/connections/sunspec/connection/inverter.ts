849e81b48e8fc40e1b2d51127b475a927da3eebf
Nathan Sullivan <nathan@nightsys.net>
2025-04-09 07:03:06 +1000

    need to import StorageModelWrite also

--- src/connections/sunspec/connection/inverter.ts
+++ src/connections/sunspec/connection/inverter.ts
@@ -11,7 +11,10 @@ import { type SettingsModel } from '../models/settings.js';
 import { settingsModel } from '../models/settings.js';
 import { type StatusModel } from '../models/status.js';
 import { statusModel } from '../models/status.js';
-import { type StorageModel } from '../models/storage.js';
+import {
+  type StorageModel,
+  type StorageModelWrite,
+} from '../models/storage.js';
 import { storageModel } from '../models/storage.js';
 import { SunSpecConnection } from './base.js';
 
